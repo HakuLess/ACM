12b5ca42179f4e68f21d557d09bd7aa289adb82d
mashuangfei <mashuangfei@meituan.com>
2021-10-13 16:10:21 +0800

    update

--- src/main/kotlin/codeforces/round744/E1.kt
+++ src/main/kotlin/codeforces/round744/E1.kt
@@ -1,13 +1,15 @@
 package codeforces.round744
 
+import java.util.*
+
 fun main(args: Array<String>) {
     val t = readLine()!!.trim().toInt()
     repeat(t) {
         val n = readLine()!!.trim().toInt()
         val arr = readLine()!!.trim().split(" ").map { it.toInt() }
-        val ans = ArrayList<Int>()
+        val ans = LinkedList<Int>()
         arr.forEach {
-            if (ans.isEmpty() || it < ans.first()) {
+            if (ans.isEmpty() || it <= ans.first()) {
                 ans.add(0, it)
             } else {
                 ans.add(it)
--- src/main/kotlin/leetcode/contest/c262/A.kt
+++ src/main/kotlin/leetcode/contest/c262/A.kt
@@ -2,26 +2,10 @@ package leetcode.contest.c262
 
 class Solution5894 {
     fun twoOutOfThree(nums1: IntArray, nums2: IntArray, nums3: IntArray): List<Int> {
-        val set = HashSet<Int>()
-        nums1.forEach {
-            set.add(it)
-        }
-        nums2.forEach {
-            set.add(it)
-        }
-        nums3.forEach {
-            set.add(it)
-        }
-        val ans = arrayListOf<Int>()
-        set.forEach {
-            if (it in nums1 && it in nums2) {
-                ans.add(it)
-            } else if (it in nums1 && it in nums3) {
-                ans.add(it)
-            } else if (it in nums2 && it in nums3) {
-                ans.add(it)
-            }
-        }
-        return ans
+        val ans = hashSetOf<Int>()
+        ans.addAll(nums1.intersect(nums2.toList()))
+        ans.addAll(nums2.intersect(nums3.toList()))
+        ans.addAll(nums1.intersect(nums3.toList()))
+        return ans.toList()
     }
 }
