1565ec23d0f2b96aaf25a121a447ae3e29b8bb75
mashuangfei <mashuangfei@meituan.com>
2021-10-19 17:56:37 +0800

    add

--- src/main/kotlin/leetcode/contest/c263/D.kt
+++ src/main/kotlin/leetcode/contest/c263/D.kt
@@ -8,7 +8,7 @@ import java.util.*
 fun main() {
     val s = Solution5905()
     s.secondMinimum(5, "[[1,2],[1,3],[1,4],[3,4],[4,5]]".toGrid(), 3, 5).print()
-    s.secondMinimum(2, "[[1,2]]".toGrid(), 3, 2).print()
+//    s.secondMinimum(2, "[[1,2]]".toGrid(), 3, 2).print()
 }
 
 class Solution5905 {
@@ -18,42 +18,35 @@ class Solution5905 {
             g.addEdge(it[0] - 1, it[1] - 1, time)
         }
 
-        val seen = IntArray(n)
-        val meet = IntArray(n) { -1 }
+        val seen = Array(n) { intArrayOf(Int.MAX_VALUE, Int.MAX_VALUE) }
 
         val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
         pq.offer(Pair(0, 0))
 
         var max = -1
         while (pq.isNotEmpty()) {
-            val item = pq.poll()
-//            println("进入 $item, 过去访问状态${seen[item.first]}")
-            if (meet[item.first] == item.second) continue
-            seen[item.first]++
-            meet[item.first] = item.second
+            val (item, curTime) = pq.poll()
+            if (seen[item][1] < curTime) continue
 
-            if (item.first == n - 1) {
+            if (item == n - 1) {
                 if (max == -1) {
-                    max = item.second
-                } else if (item.second != max) {
-                    return item.second
+                    max = curTime
+                } else if (curTime != max) {
+                    return curTime
                 }
             }
-            if ((item.second / change) % 2 != 0) {
-                // 不可移动
-                val wait = (item.second / change + 1) * change
-//                println("红灯等待 offer ${Pair(item.first, wait)}")
-                g.adj[item.first].forEach {
-//                    println("红灯后，绿灯移动 offer ${Pair(it, wait + time)}")
-                    if (seen[it] <= 2)
-                        pq.offer(Pair(it, wait + time))
-                }
-            } else {
-                // 可以移动
-                g.adj[item.first].forEach {
-//                    println("绿灯移动 offer ${Pair(it, item.second + time)}")
-                    if (seen[it] <= 2)
-                        pq.offer(Pair(it, item.second + time))
+            var nextTime = curTime + time
+            if ((curTime / change) % 2 != 0) {
+                // 不可移动，等待到下次绿灯
+                nextTime = (curTime / change + 1) * change + time
+            }
+            g.adj[item].forEach {
+                if (nextTime < seen[it][0]) {
+                    pq.offer(Pair(it, nextTime))
+                    seen[it][0] = nextTime
+                } else if (nextTime > seen[it][0] && nextTime < seen[it][1]) {
+                    pq.offer(Pair(it, nextTime))
+                    seen[it][1] = nextTime
                 }
             }
         }
