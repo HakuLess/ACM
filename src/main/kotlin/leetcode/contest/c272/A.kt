cd98c396cf92d167b943557bd295833cec92a014
mashuangfei <mashuangfei@meituan.com>
2021-12-21 17:37:38 +0800

    add

--- src/main/kotlin/leetcode/contest/c272/A.kt
+++ src/main/kotlin/leetcode/contest/c272/A.kt
@@ -2,9 +2,6 @@ package leetcode.contest.c272
 
 class SolutionA {
     fun firstPalindrome(words: Array<String>): String {
-        for (i in words.indices) {
-            if (words[i].reversed() == words[i]) return words[i]
-        }
-        return ""
+        return words.firstOrNull { it == it.reversed() } ?: ""
     }
 }
