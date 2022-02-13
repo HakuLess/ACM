04cb502269ff2fc1aca8106e52a2552edcb3e2f2
HaKu <335279429@qq.com>
2022-02-13 12:36:42 +0800

    update

--- src/main/kotlin/leetcode/contest/c280/D.kt
+++ src/main/kotlin/leetcode/contest/c280/D.kt
@@ -1,7 +1,6 @@
 package leetcode.contest.c280
 
 import utils.print
-import kotlin.math.pow
 
 fun main() {
     val s = SolutionD()
@@ -17,37 +16,12 @@ class SolutionD {
             if (c >= nums.size) continue
             for (j in 0 until numSlots * 2) {
                 if (i and (1 shl j) == 0) {
-                    val s = i or (1 shl j)
-                    dp[s] = maxOf(dp[s], dp[i] + (j / 2 + 1 and nums[c]))
-                    ans = maxOf(ans, dp[s])
+                    val k = i or (1 shl j)
+                    dp[k] = maxOf(dp[k], dp[i] + (j / 2 + 1 and nums[c]))
+                    ans = maxOf(ans, dp[k])
                 }
             }
         }
         return ans
     }
-
-//    fun maximumANDSum(nums: IntArray, numSlots: Int): Int {
-//        val n = numSlots
-//        var ans = 0
-//        val state = IntArray(n)
-//        val seen = HashSet<String>()
-//        fun dfs(index: Int, cur: Int, st: Int) {
-//            val key = "$index $cur $st"
-//            if (key in seen) return
-//            seen.add(key)
-//            if (index !in nums.indices) {
-//                ans = maxOf(ans, cur)
-//                return
-//            }
-//            for (i in 0 until n) {
-//                if (state[i] == 0 || state[i] == 1) {
-//                    state[i]++
-//                    dfs(index + 1, cur + (nums[index] and (i + 1)), st + 3.0.pow(i.toDouble()).toInt())
-//                    state[i]--
-//                }
-//            }
-//        }
-//        dfs(0, 0, 0)
-//        return ans
-//    }
 }
