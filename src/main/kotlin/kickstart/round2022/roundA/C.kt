f757c0ce302dbfc0c04be4cf757f7c78b279f510
HaKu <335279429@qq.com>
2022-03-29 13:45:01 +0800

    add

--- src/main/kotlin/kickstart/round2022/roundA/C.kt
+++ src/main/kotlin/kickstart/round2022/roundA/C.kt
@@ -1,83 +1,40 @@
 package kickstart.round2022.roundA
 
-import utils.longestPalindrome
-import utils.print
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
-
 // 通配过后，没有长度大于等于5的回文子字符串
 fun main() {
 
-    fun dfs(arr: CharArray, i: Int): Boolean {
-        if (i !in arr.indices) {
-//            println("${arr.joinToString("")} with ${arr.joinToString("").longestPalindrome()}")
-            return (arr.joinToString("").longestPalindrome().length < 5)
+    var ans = false
+    fun dfs(arr: CharArray, i: Int) {
+        if (ans) {
+            return
         }
-        var ans = false
-        if (arr[i] == '?') {
-//            println("${arr.joinToString("")}")
+        if (i == arr.size) {
+            ans = true
+            return
+        }
+        if (arr[i] != '?') {
+            if (i >= 4 && arr[i] == arr[i - 4] && arr[i - 1] == arr[i - 3]) {
+                return
+            }
+            if (i >= 5 && arr[i] == arr[i - 5] && arr[i - 1] == arr[i - 4] && arr[i - 2] == arr[i - 3]) {
+                return
+            }
+            dfs(arr, i + 1)
+        } else {
             arr[i] = '0'
-            ans = ans or dfs(arr.clone(), i + 1)
+            dfs(arr, i)
             arr[i] = '1'
-            ans = ans or dfs(arr.clone(), i + 1)
-            return ans
-        }
-        return dfs(arr, i + 1)
-    }
-
-    val blocks = arrayListOf<String>()
-
-    val arr = ArrayList<Char>()
-    val queue: Queue<ArrayList<Char>> = LinkedList<ArrayList<Char>>()
-    queue.offer(arr)
-    val seen = HashSet<String>()
-    var step = 0
-    var c = 0
-    while (queue.isNotEmpty()) {
-        val size = queue.size
-        step++
-        if (step == 11) break
-        for (i in 0 until size) {
-            val item = queue.poll()
-            if (item.isNotEmpty()) {
-                if (dfs(item.joinToString("").toCharArray(), 0) == false) {
-//                    println("${item.joinToString("")}")
-                    blocks.add("${item.joinToString("")}")
-                    c++
-                }
-            }
-            val clone0 = item.clone() as ArrayList<Char>
-            val clone1 = item.clone() as ArrayList<Char>
-            val clone2 = item.clone() as ArrayList<Char>
-            clone0.add('0')
-            clone1.add('1')
-            clone2.add('?')
-            if (clone0.joinToString() !in seen) {
-                queue.offer(clone0)
-                seen.add(clone0.joinToString())
-            }
-            if (clone1.joinToString() !in seen) {
-                queue.offer(clone1)
-                seen.add(clone1.joinToString())
-            }
-            if (clone2.joinToString() !in seen) {
-                queue.offer(clone2)
-                seen.add(clone2.joinToString())
-            }
+            dfs(arr, i)
+            arr[i] = '?'
         }
     }
-//    println(c)
-    blocks.forEach {
-        it.print()
-    }
-
 
     val t = readLine()!!.trim().toInt()
     repeat(t) {
         val n = readLine()!!.toInt()
         val s = readLine()!!.toString()
-        val ans = blocks.all { it !in s }
+        ans = false
+        dfs(s.toCharArray(), 0)
         if (ans) {
             println("Case #${it + 1}: POSSIBLE")
         } else {
--- src/main/kotlin/kickstart/round2022/roundA/D.kt
+++ src/main/kotlin/kickstart/round2022/roundA/D.kt
@@ -1,27 +1,70 @@
 package kickstart.round2022.roundA
 
 import utils.gcd
+import utils.print
 
 fun main() {
+    val seen = HashMap<String, Long>()
+
+    // 计算可被整除数量
+    fun dfs(
+        str: String,
+        i: Int = 0,
+        preSum: Long = 0L,
+        prePro: Long = 1L,
+        all: Boolean = false,
+        meetNoZero: Boolean = false
+    ): Long {
+        val key = "$i,$preSum,$prePro,$all"
+        if (key in seen) {
+            return seen[key]!!
+        }
+        if (i !in str.indices) {
+            if (preSum == 0L) return 0
+            if (prePro % preSum == 0L) {
+                return 1
+            } else {
+                return 0
+            }
+        }
+        var ans = 0L
+        if (all) {
+            if (meetNoZero) {
+                ans += dfs(str, i + 1, preSum + 0, prePro * 0, true, true)
+            } else {
+                ans += dfs(str, i + 1, preSum, prePro, true, false)
+            }
+            for (c in 1..9) {
+                ans += dfs(str, i + 1, preSum + c, prePro * c, true, true)
+            }
+        } else {
+            val max = str[i] - '0'
+            for (c in 0..max) {
+                if (c == 0) {
+                    if (meetNoZero) {
+                        ans += dfs(str, i + 1, preSum + c, prePro * c, c != max, true)
+                    } else {
+                        ans += dfs(str, i + 1, preSum, prePro, c != max, false)
+                    }
+                } else {
+                    ans += dfs(str, i + 1, preSum + c, prePro * c, c != max, true)
+                }
+            }
+        }
+        return ans.also {
+            seen[key] = it
+        }
+    }
+
     val t = readLine()!!.trim().toInt()
     repeat(t) {
         val (a, b) = readLine()!!.split(" ").map { it.toLong() }
-        var ans = 0
-        for (i in a..b) {
-            var sum = 0L
-            i.toString().forEach {
-                sum += it - '0'
-            }
-
-            var tmp = sum
-            i.toString().forEach {
-                tmp /= gcd(tmp, 0L + (it - '0'))
-            }
-            if (tmp == 1L) {
-//                println("$i is p")
-                ans++
-            }
-        }
+        seen.clear()
+        val max = dfs(b.toString())
+        seen.clear()
+        val min = dfs((a - 1).toString())
+        seen.clear()
+        val ans = max - min
         println("Case #${it + 1}: $ans")
     }
 }
