cf14e24d1ee9c4822ea57fe2463bc9cbea843048
HaKu <335279429@qq.com>
2022-11-20 12:27:54 +0800

    add

--- src/main/kotlin/leetcode/contest/c320/C.kt
+++ src/main/kotlin/leetcode/contest/c320/C.kt
@@ -3,18 +3,12 @@ package leetcode.contest.c320
 import utils.Graph
 import utils.print
 import utils.toGrid
-import kotlin.math.ceil
 
 fun main() {
     val s = SolutionC()
     s.minimumFuelCost("[[3,1],[3,2],[1,0],[0,4],[0,5],[4,6]]".toGrid(), 2).print()
     // 16
     s.minimumFuelCost("[[0,1],[2,1],[3,2],[4,2],[4,5],[6,0],[5,7],[8,4],[9,2]]".toGrid(), 2).print()
-    // 19
-    s.minimumFuelCost(
-        "[[0,1],[1,2],[1,3],[4,2],[5,3],[6,3],[6,7],[8,6],[9,0],[5,10],[11,9],[12,5],[5,13],[8,14],[11,15],[8,16],[17,0],[18,7]]".toGrid(),
-        13
-    ).print()
 }
 
 class SolutionC {
@@ -26,19 +20,22 @@ class SolutionC {
         }
         val seen = HashSet<Int>()
 
+        var ans = 0L
+
         // 人数、距离
-        fun dfs(cur: Int): Pair<Long, Long> {
-            if (cur in seen) return Pair(0, 0)
+        fun dfs(cur: Int): Int {
+            if (cur in seen) return 0
             seen.add(cur)
-            var c = 0L
-            var dis = 0L
+            var c = 0
             g.adj[cur].forEach {
-                val (p, d) = dfs(it)
+                val p = dfs(it)
                 c += p
-                dis += d + ceil(p.toDouble() / seats).toLong()
+                // 向上取整
+                ans += (p + seats - 1) / seats
             }
-            return Pair(c + 1, dis)
+            return c + 1
         }
-        return dfs(0).second
+        dfs(0)
+        return ans
     }
 }
