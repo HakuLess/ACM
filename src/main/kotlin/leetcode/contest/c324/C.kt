72baaffa929fe7fe2f0d2f1b89a069e56328c78f
HaKu <335279429@qq.com>
2022-12-20 16:39:35 +0800

    update

--- src/main/kotlin/leetcode/contest/c324/C.kt
+++ src/main/kotlin/leetcode/contest/c324/C.kt
@@ -6,7 +6,9 @@ import utils.toGrid
 fun main() {
     val s = SolutionC()
 //    s.isPossible(4, "[[1,2],[2,3],[3,4],[4,1],[1,3]]".toGrid().toList().map { it.toList() }).print()
-    s.isPossible(6, "[[1,2],[1,3],[1,4],[4,5],[5,6]]".toGrid().toList().map { it.toList() }).print()
+//    s.isPossible(6, "[[1,2],[1,3],[1,4],[4,5],[5,6]]".toGrid().toList().map { it.toList() }).print()
+    s.isPossible(5, "[[1,2],[2,3],[3,4],[4,2],[1,4],[2,5]]".toGrid().toList().map { it.toList() }).print()
+    s.isPossible(5, "[[2,3],[5,1],[5,4],[3,4],[2,4],[1,2],[2,5]]".toGrid().toList().map { it.toList() }).print()
 }
 
 class SolutionC {
@@ -27,8 +29,11 @@ class SolutionC {
         if (set.size !in arrayOf(0, 2, 4)) return false
         if (set.size == 0) return true
         if (set.size == 2) {
-            if ("${set[0]},${set[1]}" in seen && n == 2) return false
-            return true
+            if ("${set[0]},${set[1]}" !in seen) return true
+            if ((1..d.size).any {
+                    it != set[0] && it != set[1] && "$it,${set[0]}" !in seen && "$it,${set[1]}" !in seen
+                }) return true
+            return false
         }
         if (set.size == 4) {
             if ("${set[0]},${set[1]}" !in seen && "${set[2]},${set[3]}" !in seen) return true
