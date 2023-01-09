9ffb4f6599eab664f582a83df08ac1d414789a76
mashuangfei <mashuangfei@meituan.com>
2023-01-09 14:24:39 +0800

    add

--- src/main/kotlin/leetcode/contest/c327/D.kt
+++ src/main/kotlin/leetcode/contest/c327/D.kt
@@ -12,105 +12,71 @@ fun main() {
 
 class SolutionD {
     fun findCrossingTime(n: Int, k: Int, time: Array<IntArray>): Int {
-        // 编号 当前时间 效率
-        val pq0 = PriorityQueue<Triple<Int, Int, IntArray>>(compareBy({ -(it.third[0] + it.third[2]) }, { -it.first }))
-        // 左边工人
-        val pql = PriorityQueue<Triple<Int, Int, IntArray>>(
-            compareBy(
-                { it.second },
-                { -(it.third[0] + it.third[2]) },
-                { -it.first })
-        )
-        // 左边等待过桥
-        val pql1 = PriorityQueue<Triple<Int, Int, IntArray>>(compareBy({ -(it.third[0] + it.third[2]) }, { -it.first }))
-
-        time.forEachIndexed { index, ints ->
-            pq0.offer(Triple(index, 0, ints))
+        val workers = arrayOfNulls<Worker>(k)
+        for (i in 0 until k) {
+            val worker = Worker(
+                id = i,
+                l2r = time[i][0],
+                r2l = time[i][2],
+                pickOld = time[i][1],
+                putNew = time[i][3],
+                priority = (time[i][0] + time[i][2]) * 10000 + i
+            )
+            workers[i] = worker
         }
 
-        var left = n
-        for (i in 0 until minOf(n, k)) {
-            println("初始装入 ${pq0.peek()}")
-            pql.offer(pq0.poll())
-            left--
-        }
+        val lpq = PriorityQueue<Event>(compareBy { it.time })
+        val rpq = PriorityQueue<Event>(compareBy { it.time })
+        val lBridgePQ = PriorityQueue<Worker>(compareBy { -it.priority })
+        val rBridgePQ = PriorityQueue<Worker>(compareBy { -it.priority })
+        lBridgePQ.addAll(workers)
 
-        // 过河时间
-        var cur = 0
-
-        // 右边工人
-        val pqr = PriorityQueue<Triple<Int, Int, IntArray>>(
-            compareBy(
-                { it.second },
-                { -(it.third[0] + it.third[2]) },
-                { -it.first })
-        )
-        // 右边等待过桥
-        val pqr1 = PriorityQueue<Triple<Int, Int, IntArray>>(compareBy({ -(it.third[0] + it.third[2]) }, { -it.first }))
-
-        // 等待搬货工人
-        val pq = PriorityQueue<Triple<Int, Int, IntArray>>(
-            compareBy(
-                { it.second },
-                { -(it.third[0] + it.third[2]) },
-                { -it.first })
-        )
-
-        while ((pql.isNotEmpty() || pqr.isNotEmpty() || pql1.isNotEmpty() || pqr1.isNotEmpty()) || left != 0) {
-
-            while (pq.isNotEmpty() && left != 0 && cur >= pq.peek().second) {
-                println("${pq.joinToString()}")
-                val item = pq.poll()
-                println("左侧 ${item.first} 开始搬货，剩余货物 $left")
-                pql.offer(item)
-                left--
+        var goods = n
+        var now = 0L
+        while (goods + rBridgePQ.size + rpq.size > 0) {
+            // 左侧放完东西的 等待入队
+            while (!lpq.isEmpty() && lpq.peek().time <= now) {
+                lBridgePQ.add(lpq.remove().worker)
+            }
+            // 右侧拿完东西的 等待入队
+            while (!rpq.isEmpty() && rpq.peek().time <= now) {
+                rBridgePQ.add(rpq.remove().worker)
             }
 
-            // 右侧工人进入等待过桥
-            while (pqr.isNotEmpty()) {
-                if (pqr.peek().second <= cur) {
-                    pqr1.offer(pqr.poll())
-                } else {
-                    break
+            if (!rBridgePQ.isEmpty()) {
+                // 右侧优先使用桥
+                val head = rBridgePQ.remove()
+                now += head.r2l.toLong()
+                lpq.add(Event(now + head.putNew, head))
+            } else if (!lBridgePQ.isEmpty() && goods > 0) {
+                // 左侧使用桥
+                goods--
+                val head = lBridgePQ.remove()
+                now += head.l2r.toLong()
+                rpq.add(Event(now + head.pickOld, head))
+            } else {
+                // 没人用桥 时间快进到下一个等待桥的时间
+                var minTime = Long.MAX_VALUE
+                if (!lpq.isEmpty()) {
+                    minTime = minOf(minTime, lpq.peek().time)
                 }
-            }
-            // 左侧工人进入等待过桥
-            while (pql.isNotEmpty()) {
-                if (pql.peek().second <= cur) {
-                    pql1.offer(pql.poll())
-                } else {
-                    break
+                if (!rpq.isEmpty()) {
+                    minTime = minOf(minTime, rpq.peek().time)
                 }
-            }
-
-//            println("当前状态 等待中 ${pq.size}   左侧等待过桥 ${pql.size}   ${pqr.size} ${pql1.size} ${pqr1.size} $left")
-
-            if (pql1.isEmpty() && pqr1.isEmpty() && (pql.isNotEmpty() || pqr.isNotEmpty()) || (left != 0 && pq.isNotEmpty())) {
-                cur++
-                println("无人等待过桥，当前时间设置为下次工人时间 $cur")
-//                println("${pql1.size} ${pqr1.size} ${pql.size} ${pqr.size}")
-            }
-
-            if (pqr1.isNotEmpty()) {
-                // 右边等待的先过桥
-                val item = pqr1.poll()
-                // 设置当前时间
-                cur = maxOf(cur, item.second)
-                println("右侧 ${item.first} 过桥，当前时间变为 $cur 占用桥$cur..${cur + item.third[2]}")
-                cur += item.third[2]
-
-                // 进入等待搬货
-                pq.offer(Triple(item.first, cur + item.third[3], item.third))
-
-            } else if (pql1.isNotEmpty()) {
-                val item = pql1.poll()
-                // 设置当前时间
-                cur = maxOf(cur, item.second)
-                cur += item.third[0]
-                println("左侧 ${item.first} 过桥，当前时间变为 $cur")
-                pqr.offer(Triple(item.first, cur + item.third[1], item.third))
+                now = maxOf(now, minTime)
             }
         }
-        return cur
+        return now.toInt()
     }
 }
+
+data class Worker(
+    var id: Int,
+    var l2r: Int,
+    var r2l: Int,
+    var pickOld: Int,
+    var putNew: Int,
+    var priority: Int
+)
+
+data class Event(var time: Long, var worker: Worker)
