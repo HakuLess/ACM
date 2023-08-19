345a855d83a7d332691ca64f1d65dbb6350a21a2
HaKu <335279429@qq.com>
2023-08-20 00:46:30 +0800

    add

--- src/main/kotlin/leetcode/contest/b111/D.kt
+++ src/main/kotlin/leetcode/contest/b111/D.kt
@@ -4,56 +4,48 @@ import utils.print
 
 fun main() {
     val s = SolutionD()
-    s.numberOfBeautifulIntegers(10, 20, 3).print()
+//    s.numberOfBeautifulIntegers(10, 20, 3).print()
     s.numberOfBeautifulIntegers(1, 10, 1).print()
-    s.numberOfBeautifulIntegers(5, 5, 2).print()
+//    s.numberOfBeautifulIntegers(5, 5, 2).print()
 }
 
 class SolutionD {
     fun numberOfBeautifulIntegers(low: Int, high: Int, k: Int): Int {
-        fun countDigits(n: Int): IntArray {
-            val counts = IntArray(10)
-            var num = n
-            while (num > 0) {
-                val digit = num % 10
-                counts[digit]++
-                num /= 10
-            }
-            return counts
-        }
+        val seen = HashMap<String, Int>()
 
-        val dp = Array(20) { IntArray(k) { 0 } }
-        dp[1][0] = 1
+        fun getRes(v: Int): Int {
+            val s = v.toString()
 
-        for (i in 2 until 20) {
-            for (j in 0 until k) {
-                for (d in 0..9) {
-                    if (i % 2 == 1 || d % 2 == 1) {
-                        dp[i][(j + d) % k] += dp[i - 1][j]
-                    }
+            fun dfs(idx: Int, start: Boolean, bound: Boolean, mod: Int, diff: Int): Int {
+                val key = "$idx,$start,$bound,$mod,$diff"
+                if (key in seen) return seen[key]!!
+                if (idx == s.length) {
+                    return if (mod == 0 && diff == 0 && start) 1
+                    else 0
+                }
+
+                var v = if (start) 0 else dfs(idx + 1, false, false, 0, 0)
+
+                for (i in if (bound) 0..(s[idx] - '0') else 0..9) {
+                    if (!start && i == 0) continue
+
+                    val newBound = bound && i == s[idx] - '0'
+                    val newMod = (10 * mod + i) % k
+                    val newDiff = if (i % 2 == 0) diff + 1 else diff - 1
+                    v += dfs(idx + 1, true, newBound, newMod, newDiff)
+                }
+                return v.also {
+                    seen[key] = it
                 }
             }
-        }
 
-        val lowCounts = countDigits(low - 1)
-        val highCounts = countDigits(high)
-
-        fun getSum(arr: IntArray): Int {
-            var sum = 0
-            for (i in arr) {
-                sum += i
+            return dfs(0, false, true, 0, 0).also {
+                seen.clear()
             }
-            return sum
         }
 
-        val lowSum = getSum(dp[lowCounts.size])
-        val highSum = getSum(dp[highCounts.size])
-
-        var count = highSum - lowSum
-        if (lowCounts.sum() % 2 == 0 && low % k == 0) {
-            count++
-        }
-
-        return count
+        val a = getRes(high)
+        seen.clear()
+        return a - getRes(low - 1)
     }
 }
