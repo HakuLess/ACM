9f5cdd453affe25570333da0f587c45671766361
HaKu <335279429@qq.com>
2024-06-09 00:16:41 +0800

    add

--- src/main/kotlin/leetcode/contest/b132/D.kt
+++ src/main/kotlin/leetcode/contest/b132/D.kt
@@ -4,32 +4,41 @@ import utils.print
 
 fun main() {
     val s = SolutionD()
-    s.maximumLength(intArrayOf(1, 2, 1, 1, 3), 2).print()
+    // 4
+//    s.maximumLength(intArrayOf(1, 2, 1, 1, 3), 2).print()
+//    // 2
 //    s.maximumLength(intArrayOf(1, 2, 3, 4, 5, 1), 0).print()
+//
+//    //
+//    s.maximumLength(intArrayOf(2, 15), 2).print()
+
+    s.maximumLength(intArrayOf(29, 30, 30), 0).print()
+//    s.maximumLength(intArrayOf(7, 19, 17, 29), 1).print()
+
 }
 
 class SolutionD {
     fun maximumLength(nums: IntArray, k: Int): Int {
-        // 已有K个 num结尾的最大 长度
-        val mapArr = Array<HashMap<Int, Int>>(k + 1) { HashMap() }
-        var ans = 0
-        for (i in nums.indices) {
-            val cur = nums[i]
-            for (j in mapArr.indices.reversed()) {
-                if (j + 1 in mapArr.indices) {
-                    mapArr[j].forEach { (k, v) ->
-                        // 不相同，尝试增加
-                        if (k != cur) {
-                            mapArr[j + 1][cur] = maxOf(mapArr[j + 1].getOrDefault(cur, 0), v + 1)
-                            ans = maxOf(ans, mapArr[j + 1][cur]!!)
-                        }
-                    }
-                }
+        val n = nums.size
+        val dp = Array(n + 1) { Array(k + 2) { IntArray(2) } }
+        val map = HashMap<Int, Int>()
 
-                mapArr[j][cur] = mapArr[j].getOrDefault(cur, 0) + 1
-                ans = maxOf(ans, mapArr[j][cur]!!)
+        for (i in 1..n) {
+            for (j in 1..k + 1) {
+                // 到位置i，不以当前元素结尾
+                dp[i][j][0] = maxOf(dp[i - 1][j][0], dp[i - 1][j][1])
+                // 到位置i，以当前元素结尾
+                dp[i][j][1] = maxOf(dp[i - 1][j - 1][0], dp[i - 1][j - 1][1]) + 1
+
+                val index = map[nums[i - 1]]
+                if (index != null) {
+                    // 到位置i，以当前元素结尾 上一个相同元素结尾值+1
+                    dp[i][j][1] = maxOf(dp[i][j][1], dp[index + 1][j][1] + 1)
+                }
             }
+            map[nums[i - 1]] = i - 1
         }
-        return ans
+
+        return maxOf(dp[n][k + 1][0], dp[n][k + 1][1])
     }
 }
