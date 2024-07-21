d19fc9505acbdf830ec3080314a08747ebf31e3c
HaKu <335279429@qq.com>
2024-07-21 12:20:37 +0800

    add

--- src/main/kotlin/leetcode/contest/c407/D.kt
+++ src/main/kotlin/leetcode/contest/c407/D.kt
@@ -18,42 +18,64 @@ fun main() {
 class SolutionD {
     fun minimumOperations(nums: IntArray, target: IntArray): Long {
         val n = nums.size
-        val det = LongArray(n)
+        val diff = LongArray(n)
         for (i in 0 until n) {
-            det[i] = (target[i] - nums[i]).toLong()
+            diff[i] = (target[i] - nums[i]).toLong()
         }
-
-        // 返回值的符号
-        fun sgn(x: Long): Int {
-            return when {
-                x == 0L -> 0
-                x > 0L -> 1
-                else -> -1
+        val dp = LongArray(n)
+        dp[0] = abs(diff[0])
+        for (i in 1 until n) {
+            dp[i] = dp[i - 1]
+            if (diff[i] <= diff[i - 1] && diff[i - 1] <= 0) {
+                dp[i] += diff[i - 1] - diff[i]
+            } else if (diff[i] >= diff[i - 1] && diff[i - 1] >= 0) {
+                dp[i] += diff[i] - diff[i - 1]
+//            } else if ((diff[i - 1] <= 0 && diff[i] >= 0) || (diff[i - 1] >= 0 && diff[i] <= 0)) {
+            } else if (diff[i - 1] * diff[i] <= 0) {
+                dp[i] += abs(diff[i])
             }
         }
-
-        // 计算从 l 到 r 区间的最小操作数
-        fun helper(l: Int, r: Int): Long {
-            var ret = 0L
-            var last = 0L
-            for (i in l..r) {
-                val x = abs(det[i])
-                if (x > last) ret += x - last
-                last = x
-            }
-            return ret
-        }
-
-        var ans = 0L
-        // 处理每一段的正数和负数
-        var j = 0
-        for (i in 0 until n) {
-            if (i == n - 1 || sgn(det[i + 1]) != sgn(det[j])) {
-                ans += helper(j, i)
-                j = i + 1
-            }
-        }
-
-        return ans
+        return dp[n - 1]
     }
+
+//    fun minimumOperations(nums: IntArray, target: IntArray): Long {
+//        val n = nums.size
+//        val det = LongArray(n)
+//        for (i in 0 until n) {
+//            det[i] = (target[i] - nums[i]).toLong()
+//        }
+//
+//        // 返回值的符号
+//        fun sgn(x: Long): Int {
+//            return when {
+//                x == 0L -> 0
+//                x > 0L -> 1
+//                else -> -1
+//            }
+//        }
+//
+//        // 计算从 l 到 r 区间的最小操作数
+//        fun helper(l: Int, r: Int): Long {
+//            var ret = 0L
+//            var last = 0L
+//            for (i in l..r) {
+//                val x = abs(det[i])
+//                if (x > last) ret += x - last
+//                last = x
+//            }
+//            return ret
+//        }
+//
+//        var ans = 0L
+//        // 处理每一段的正数和负数
+//        var j = 0
+//        for (i in 0 until n) {
+//            if (i == n - 1 || sgn(det[i + 1]) != sgn(det[j])) {
+//                ans += helper(j, i)
+//                j = i + 1
+//            }
+//        }
+//
+//        return ans
+//    }
 }
