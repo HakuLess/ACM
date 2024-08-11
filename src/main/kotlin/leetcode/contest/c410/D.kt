44e3451721cf8dfa0f348db5dbe53d162727ed35
HaKu <335279429@qq.com>
2024-08-11 12:38:00 +0800

    update

--- src/main/kotlin/leetcode/contest/c410/D.kt
+++ src/main/kotlin/leetcode/contest/c410/D.kt
@@ -6,46 +6,45 @@ fun main() {
     val s = SolutionD()
     s.countOfPairs(intArrayOf(2, 3, 2)).print()
     s.countOfPairs(intArrayOf(5, 5, 5, 5)).print()
-    val arr = ArrayList<Int>()
-    repeat(300) {
-        arr.add(1000)
-    }
-    arr.joinToString(",").print()
-    s.countOfPairs(arr.toIntArray()).print()
+//    val arr = ArrayList<Int>()
+//    repeat(300) {
+//        arr.add(1000)
+//    }
+//    arr.joinToString(",").print()
+//    s.countOfPairs(arr.toIntArray()).print()
 }
 
 class SolutionD {
     fun countOfPairs(nums: IntArray): Int {
+        val n = nums.size
         val mod = 1000000007L
-        var dp = LongArray(1001)
-        var isFirst = true
-        var last = -1
-        for (n in nums) {
-            val nextDp = LongArray(n + 1)
-            if (isFirst) {
-                for (i in 0..n) {
-                    nextDp[i] = 1
-                }
-            } else {
-                var sum = 0L
-                var st = 0
-                for (i in 0..n) {
-                    val curArr2 = n - i
-                    while (st <= minOf(i, last) && last - st >= curArr2) {
-                        sum = (sum + dp[st]) % mod
-                        ++st
-                    }
-                    nextDp[i] = sum
+        val maxNum = nums.maxOrNull()!!
+
+        var dp = LongArray(maxNum + 1) { 1L }
+
+        for (i in 1 until n) {
+            val pre = LongArray(maxNum + 1)
+            pre[0] = dp[0]
+            for (j in 1..maxNum) {
+                pre[j] = (pre[j - 1] + dp[j]) % mod
+            }
+
+            val newDp = LongArray(maxNum + 1) { 0L }
+            for (j in 0..nums[i]) {
+                // 数组2保持非递增
+                val r = minOf(j, nums[i - 1] - (nums[i] - j))
+                if (r >= 0) {
+                    newDp[j] = pre[r]
                 }
             }
-            isFirst = false
-            dp = nextDp
-            last = n
+            dp = newDp
         }
-        var res = 0L
-        for (n in dp) {
-            res = (res + n) % mod
+
+        var ans = 0L
+        dp.forEach {
+            ans += it
+            ans %= mod
         }
-        return (res % mod).toInt()
+        return (ans % mod).toInt()
     }
 }
