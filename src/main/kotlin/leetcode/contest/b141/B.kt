644c2df256fa5638ef2329ba2c3aece0cf0a8242
HaKu <335279429@qq.com>
2024-10-12 23:48:45 +0800

    update

--- src/main/kotlin/leetcode/contest/b141/B.kt
+++ src/main/kotlin/leetcode/contest/b141/B.kt
@@ -1,16 +1,29 @@
 package leetcode.contest.b141
 
-// Not Finished https://leetcode.cn/contest/biweekly-contest-141/problems/construct-the-minimum-bitwise-array-ii/
+import utils.print
+
+fun main() {
+    val s = SolutionB()
+    s.minBitwiseArray(listOf(2, 3, 5, 7)).print()
+}
+
 class SolutionB {
     fun minBitwiseArray(nums: List<Int>): IntArray {
         val n = nums.size
         val ans = IntArray(n) { -1 }
 
         for (i in 0 until n) {
-            for (x in 0..nums[i]) {
-                if ((x or (x + 1)) == nums[i]) {
-                    ans[i] = x
-                    break
+            val sb = StringBuilder(nums[i].toString(2))
+            for (j in sb.indices) {
+                if (sb[j] == '1') {
+                    sb.setCharAt(j, '0')
+                    val tmp = sb.toString().toInt(2)
+//                    println("try $tmp with ${tmp or (tmp + 1)}")
+                    if (tmp or (tmp + 1) == nums[i]) {
+                        ans[i] = tmp
+                        break
+                    }
+                    sb.setCharAt(j, '1')
                 }
             }
         }
