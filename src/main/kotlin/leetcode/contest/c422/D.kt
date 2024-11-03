d00398e197b5bb679af783928eac69b10194dd20
HaKu <335279429@qq.com>
2024-11-03 11:37:38 +0800

    update

--- src/main/kotlin/leetcode/contest/c422/D.kt
+++ src/main/kotlin/leetcode/contest/c422/D.kt
@@ -7,48 +7,52 @@ import utils.print
 fun main() {
     val s = SolutionD()
     s.countBalancedPermutations("123").print()
+    s.countBalancedPermutations("112").print()
+    s.countBalancedPermutations("12345").print()
+    s.countBalancedPermutations("7160").print()
 }
 
 class SolutionD {
     fun countBalancedPermutations(num: String): Int {
 
-//        val arr = num.toCharArray().map { ((it - '0') % 2) }.toIntArray()
-//        val a = arr.count { it % 2 == 0 }
-//        val b = arr.count { it % 2 != 0 }
-//
         val mod = 1_000_000_007L
-//
-//        var tmp = 1L
-//        for (i in 1..a) {
-//            tmp *= a
-//            tmp %= mod
-//        }
-//        for (i in 1..b) {
-//            tmp *= b
-//            tmp %= mod
-//        }
-//
-//        println("tmp is $tmp")
+        val a = num.length / 2
+        val b = num.length - a
 
-        var ans = 0L
-        num.toCharArray().map { it - '0' }.toIntArray().permuteUnique().forEach {
-            var aSum = 0
-            var bSum = 0
-            for (i in it.indices) {
-                if (i % 2 == 0) {
-                    aSum += it[i]
-                } else {
-                    bSum += it[i]
-                }
+        val arr = num.map { it - '0' }.toIntArray()
+
+        val seen = HashMap<String, Long>()
+        fun dfs(index: Int, leftA: Int, leftB: Int, sumA: Int, sumB: Int): Long {
+            if (leftA == 0 && leftB == 0) {
+                return if (sumA == sumB) 1L else 0L
             }
 
-//            println("${it.joinToString()}  for  $aSum == $bSum")
-            if (aSum == bSum) {
-                ans ++
-                ans %= mod
+            val key = "$leftA, $leftB, $sumA, $sumB"
+            if (key in seen) return seen[key]!!
+            var tmp = 0L
+            if (leftA > 0) {
+                tmp += dfs(index + 1, leftA - 1, leftB, sumA + arr[index], sumB)
+                tmp %= mod
+            }
+            if (leftB > 0) {
+                tmp += dfs(index + 1, leftA, leftB - 1, sumA, sumB + arr[index])
+                tmp %= mod
+            }
+            return tmp.also {
+                seen[key] = it
             }
         }
 
-        return (ans % mod).toInt()
+        var tmp = dfs(0, a, b, 0, 0)
+//        for (i in 1..a) {
+//            tmp *= i
+//            tmp %= mod
+//        }
+//        for (i in 1..b) {
+//            tmp *= i
+//            tmp %= mod
+//        }
+
+        return (tmp % mod).toInt()
     }
 }
