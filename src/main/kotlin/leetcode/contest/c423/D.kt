bafdc8d638c16ecfa4637b049f92c6ff7a548a1d
HaKu <335279429@qq.com>
2024-11-10 12:05:51 +0800

    add

--- src/main/kotlin/leetcode/contest/c423/D.kt
+++ src/main/kotlin/leetcode/contest/c423/D.kt
@@ -2,32 +2,38 @@ package leetcode.contest.c423
 
 class SolutionD {
     fun countKReducibleNumbers(s: String, k: Int): Int {
-        val MOD = 1_000_000_007
-        val n = s.toLong(2) // 将二进制字符串 s 转换为整数
-        val memo = mutableMapOf<Pair<Int, Int>, Boolean>()
+        val mod = 1_000_000_007L
+        val pre = IntArray(1001)
 
-        // 检查数字 x 是否可以在 k 次或更少的操作内被约简到 1
-        fun isKReducible(x: Int, remainingSteps: Int): Boolean {
-            if (x == 1) return true
-            if (remainingSteps == 0) return false
-
-            val key = Pair(x, remainingSteps)
-            if (key in memo) return memo[key]!!
-
-            val next = Integer.bitCount(x) // 计算二进制表示中的置位数
-            val result = isKReducible(next, remainingSteps - 1)
-            memo[key] = result
-            return result
+        for (i in 2 until 1001) {
+            pre[i] = pre[i.countOneBits()] + 1
         }
 
-        // 计数符合条件的整数
-        var count = 0
-        for (i in 1 until n) {
-            if (isKReducible(i.toInt(), k)) {
-                count = (count + 1) % MOD
+        val nums = s.map { it.toString().toInt() }
+        val n = nums.size
+        val dp = LongArray(n + 1)
+
+        var cur = 0
+        for (i in nums.indices) {
+            for (j in i - 1 downTo 0) {
+                dp[j + 1] += dp[j]
+                dp[j + 1] %= mod
+            }
+            if (nums[i] != 0) {
+                dp[cur] += 1L
+                dp[cur] %= mod
+            }
+            cur += nums[i]
+        }
+
+        var ans = 0L
+        for (i in 0..n) {
+            if (pre[i] < k) {
+                ans += dp[i]
+                ans %= mod
             }
         }
 
-        return count
+        return ((ans - 1 + mod) % mod).toInt()
     }
 }
