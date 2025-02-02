fd1704b13deba118ea0cf9dde917843ae63eb826
HaKu <335279429@qq.com>
2025-02-02 12:19:28 +0800

    update

--- src/main/kotlin/leetcode/contest/c435/C.kt
+++ src/main/kotlin/leetcode/contest/c435/C.kt
@@ -1,21 +1,19 @@
 package leetcode.contest.c435
 
 import utils.gcd
-import utils.lcm
-import utils.permute
 import utils.print
 import java.util.*
-import kotlin.math.abs
+
 
 fun main() {
     val s = SolutionC()
     // 1
-//    s.minimumIncrements(intArrayOf(1, 2, 3), intArrayOf(4)).print()
+    s.minimumIncrements(intArrayOf(1, 2, 3), intArrayOf(4)).print()
 //    // 2
-//    s.minimumIncrements(intArrayOf(8, 4), intArrayOf(10, 5)).print()
+    s.minimumIncrements(intArrayOf(8, 4), intArrayOf(10, 5)).print()
 //
 //    // 3
-//    s.minimumIncrements(intArrayOf(8, 10, 9), intArrayOf(10, 6, 6)).print()
+    s.minimumIncrements(intArrayOf(8, 10, 9), intArrayOf(10, 6, 6)).print()
 
     // 8
     s.minimumIncrements(intArrayOf(10, 4, 5, 10), intArrayOf(6, 8, 7, 3)).print()
@@ -24,171 +22,47 @@ fun main() {
 class SolutionC {
     fun minimumIncrements(nums: IntArray, target: IntArray): Int {
 
-
-        fun helper(target: TreeSet<Int>): Int {
-
-            val ts = TreeSet<Int>()
-            ts.addAll(nums.toHashSet())
-
-//            println("enter ${target.joinToString()}")
-
-            var ans = Int.MAX_VALUE
-
-            target.toIntArray().permute().forEach {
-
-                var tmp = 0
-
-                it.forEach {
-                    var min = Int.MAX_VALUE
-                    var cur = it
-                    var toRemove = 0
-
-                    while (cur <= 10000) {
-
-                        ts.floor(cur)?.let {
-                            if (cur - it < min) {
-                                min = cur - it
-                                toRemove = it
-                            }
-                        }
-
-                        cur += it
+        val max = 1 shl target.size
+        // 状态压缩，计算target已经被拿走的最小情况。
+        val lcm = LongArray(max)
+        for (i in 1 until max) {
+            var cur: Long = -1
+            for (j in target.indices) {
+                if (i shr j and 1 == 1) {
+                    cur = if (cur == -1L) {
+                        target[j].toLong()
+                    } else {
+                        val tmp: Long = gcd(cur, target[j].toLong())
+                        cur / tmp * target[j]
                     }
-
-                    ts.floor(cur)?.let {
-                        if (cur - it < min) {
-                            min = cur - it
-                            toRemove = it
-                        }
-                    }
-
-                    // 被其他倍数消费掉，不可改变
-                    ts.remove(toRemove)
-
-//                    if (min == Int.MAX_VALUE) {
-//                        return@forEach
-//                    }
-//                    println("find $it with $min   pre ans is $ans")
-                    tmp += min
-//                    println("ans is $ans")
-                }
-                if (tmp > 0) {
-                    ans = minOf(ans, tmp)
                 }
             }
-
-            return ans
+            lcm[i] = cur
         }
 
-        // 最多4个数字 各种组合最小公倍数
-        val n = target.size
-        val targetSet = TreeSet<Int>()
+//        lcm.print()
 
-        var ans = Int.MAX_VALUE
-        when (n) {
-            1 -> {
-                targetSet.addAll(target.toHashSet())
-                ans = helper(targetSet)
+        var dp = LongArray(max) { Long.MAX_VALUE }
+        dp[0] = 0
+        for (n in nums) {
+            val nextDp = LongArray(max) { Long.MAX_VALUE }
+            nextDp[0] = 0
+            // 符合当前target取法，已经完成的最小情况
+            for (i in 1 until max) {
+                nextDp[i] = dp[i]
+                for (j in 0 until max) {
+                    if (i != j && dp[j] != Long.MAX_VALUE && i or j == i) {
+                        val l = lcm[i xor j]
+                        if (n % l == 0L) {
+                            nextDp[i] = minOf(nextDp[i], dp[j])
+                        } else {
+                            nextDp[i] = minOf(nextDp[i], dp[j] + (n / l + 1) * l - n)
+                        }
+                    }
+                }
             }
-            2 -> {
-                val (a, b) = target
-                targetSet.addAll(target.toHashSet())
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, b))
-                ans = minOf(ans, helper(targetSet))
-            }
-
-            3 -> {
-                val (a, b, c) = target
-                targetSet.addAll(target.toHashSet())
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, b))
-                targetSet.add(c)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, c))
-                targetSet.add(b)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(b, c))
-                targetSet.add(a)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(c, lcm(a, b)))
-                ans = minOf(ans, helper(targetSet))
-            }
-
-            4 -> {
-                val (a, b, c, d) = target
-                targetSet.addAll(target.toHashSet())
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, b))
-                targetSet.add(c)
-                targetSet.add(d)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, b))
-                targetSet.add(lcm(c, d))
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, c))
-                targetSet.add(b)
-                targetSet.add(d)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(a, c))
-                targetSet.add(lcm(b, d))
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(b, c))
-                targetSet.add(a)
-                targetSet.add(d)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(b, c))
-                targetSet.add(lcm(a, d))
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(c, lcm(a, b)))
-                targetSet.add(d)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(d, lcm(a, b)))
-                targetSet.add(c)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(d, lcm(a, c)))
-                targetSet.add(b)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(d, lcm(b, c)))
-                targetSet.add(a)
-                ans = minOf(ans, helper(targetSet))
-
-                targetSet.clear()
-                targetSet.add(lcm(lcm(d, lcm(b, c)), a))
-                ans = minOf(ans, helper(targetSet))
-            }
+            dp = nextDp
         }
-
-        return ans
+        return dp[max - 1].toInt()
     }
 }
--- src/main/kotlin/leetcode/contest/c435/D.kt
+++ src/main/kotlin/leetcode/contest/c435/D.kt
@@ -2,6 +2,7 @@ package leetcode.contest.c435
 
 import utils.print
 
+
 fun main() {
     val s = SolutionD()
     // -1
@@ -14,46 +15,56 @@ fun main() {
 
 class SolutionD {
     fun maxDifference(s: String, k: Int): Int {
+        var ans = Int.MIN_VALUE
 
-        fun helper(a: Int, b: Int): Int {
+        val arr = charArrayOf('0', '1', '2', '3', '4')
+        val count = IntArray(s.length)
+        val mark = IntArray(s.length)
+        // 状态压缩10代表l出现奇数次，r出现偶数次
+        // 00代表l出现偶数次，r出现偶数次
+        // 01代表l出现偶数次，r出现奇数次
+        // 00代表l出现奇数次，r出现奇数次
+        for (l in arr) {
+            for (r in arr) {
+                if (l == r) continue
+                var sum = 0
+                var currentMark = 0
+                // lMet 和 rMet 确保区间内必须存在一个对应的数字。
+                var lMet = -1
+                var rMet = -1
+                var pos = 0
 
-//            println("enter ==== $a $b")
-
-            var ans = Int.MIN_VALUE
-
-            for (left in s.indices) {
-                var ac = 0
-                var bc = 0
-                for (right in left..s.lastIndex) {
-                    val item = s[right] - '0'
-                    if (item == a) {
-                        ac++
-                    } else if (item == b) {
-                        bc++
+                val minMem = IntArray(4) { Int.MAX_VALUE }
+                minMem[0] = 0
+                for (i in s.indices) {
+                    if (s[i] == l) {
+                        ++sum
+                        lMet = i
+                        currentMark = currentMark xor 2
+                    } else if (s[i] == r) {
+                        --sum
+                        rMet = i
+                        currentMark = currentMark xor 1
                     }
-
-                    if (right - left + 1 >= k) {
-//                        println(" ==== $left..$right $ac - $bc == ${ac - bc} with $ans")
-
-                        if (ac % 2 == 1 && bc % 2 == 0 && ac != 0 && bc != 0) {
-//                            println("$left..$right $ac - $bc == ${ac - bc} with $ans")
-                            ans = maxOf(ans, ac - bc)
+                    count[i] = sum
+                    mark[i] = currentMark
+                    if (i >= k - 1 && lMet != -1 && rMet != -1) {
+                        for (j in 0..3) {
+                            // 必须确保左数出现奇数次，右数出现偶数次。
+                            if (currentMark xor j == 2 && minMem[j] != Int.MAX_VALUE) {
+                                ans = maxOf(ans, sum - minMem[j])
+                            }
+                        }
+                        var min = minOf(i - k + 1, lMet - 1)
+                        min = minOf(min, rMet - 1)
+                        while (pos <= min) {
+                            minMem[mark[pos]] = minOf(minMem[mark[pos]], count[pos])
+                            ++pos
                         }
                     }
                 }
             }
-
-            return ans
         }
-
-        var ans = Int.MIN_VALUE
-        for (a in 0..4) {
-            for (b in 0..4) {
-                if (a == b) continue
-                ans = maxOf(ans, helper(a, b))
-            }
-        }
-
         return ans
     }
 }
