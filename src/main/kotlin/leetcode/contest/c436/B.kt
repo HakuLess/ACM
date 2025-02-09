5e92ccd4b264f6d28f20f17595d85ed7841c2eb0
HaKu <335279429@qq.com>
2025-02-09 12:18:40 +0800

    add

--- src/main/kotlin/leetcode/contest/c436/B.kt
+++ src/main/kotlin/leetcode/contest/c436/B.kt
@@ -8,34 +8,23 @@ fun main() {
 class SolutionB {
     fun assignElements(groups: IntArray, elements: IntArray): IntArray {
 
-        val maxGroupSize = groups.maxOrNull() ?: 0
-        val minIndexForGroupSize = IntArray(maxGroupSize + 1) { -1 }
+        val max = groups.maxOrNull()!! + 1
 
-        val elementToMinIndex = mutableMapOf<Int, Int>()
-        for (j in elements.indices) {
-            val element = elements[j]
-            if (element == 0) continue
-            if (!elementToMinIndex.containsKey(element) || j < elementToMinIndex[element]!!) {
-                elementToMinIndex[element] = j
-            }
-        }
-
-        for ((element, j) in elementToMinIndex) {
-            for (k in element..maxGroupSize step element) {
-                if (minIndexForGroupSize[k] == -1 || j < minIndexForGroupSize[k]) {
-                    minIndexForGroupSize[k] = j
+        val seen = HashSet<Int>()
+        val arr = IntArray(max) { -1 }
+        for (i in elements.indices) {
+            val item = elements[i]
+            if (item in seen) continue
+            seen.add(item)
+            var cur = item
+            while (cur in arr.indices) {
+                if (arr[cur] == -1) {
+                    arr[cur] = i
                 }
+                cur += item
             }
         }
 
-        val assigned = IntArray(groups.size) { -1 }
-        for (i in groups.indices) {
-            val groupSize = groups[i]
-            if (groupSize <= maxGroupSize) {
-                assigned[i] = minIndexForGroupSize[groupSize]
-            }
-        }
-
-        return assigned
+        return groups.map { arr[it] }.toIntArray()
     }
 }
