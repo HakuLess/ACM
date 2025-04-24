d5fe132c229ef64706cad784e5c36e4eb7d199cf
HaKu <335279429@qq.com>
2025-04-24 11:02:00 +0800

    update

--- src/main/kotlin/leetcode/contest/c356/B.kt
+++ src/main/kotlin/leetcode/contest/c356/B.kt
@@ -4,21 +4,34 @@ import utils.print
 
 fun main() {
     val s = SolutionB()
+    // 4
     s.countCompleteSubarrays(intArrayOf(1, 3, 1, 2, 2)).print()
+    // 10
     s.countCompleteSubarrays(intArrayOf(5, 5, 5, 5)).print()
 }
 
 class SolutionB {
     fun countCompleteSubarrays(nums: IntArray): Int {
-        val t = nums.toHashSet().size
+        val totalSize = nums.toHashSet().size
+        val n = nums.size
         var ans = 0
-        for (i in nums.indices) {
-            val set = hashSetOf<Int>(nums[i])
-            for (j in i until nums.size) {
-                set.add(nums[j])
-                if (set.size == t) {
-                    ans++
-                }
+        var right = 0
+        val map = HashMap<Int, Int>()
+        for (left in nums.indices) {
+            while (right in nums.indices && map.keys.size != totalSize) {
+                map[nums[right]] = map.getOrDefault(nums[right], 0) + 1
+                right++
+            }
+
+            if (map.keys.size != totalSize) {
+                return ans
+            }
+
+            ans += n - right + 1
+
+            map[nums[left]] = map.getOrDefault(nums[left], 0) - 1
+            if (map[nums[left]] == 0) {
+                map.remove(nums[left])
             }
         }
         return ans
