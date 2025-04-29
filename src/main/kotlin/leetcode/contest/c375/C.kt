9c58cf3056e7dc19733247a5b99e06f7e82b307e
HaKu <335279429@qq.com>
2025-04-29 11:46:41 +0800

    update

--- src/main/kotlin/leetcode/contest/c375/C.kt
+++ src/main/kotlin/leetcode/contest/c375/C.kt
@@ -1,6 +1,7 @@
 package leetcode.contest.c375
 
 import utils.print
+import java.util.*
 
 fun main() {
     val s = SolutionC()
@@ -9,19 +10,40 @@ fun main() {
 
 class SolutionC {
     fun countSubarrays(nums: IntArray, k: Int): Long {
-        val maxItem = nums.maxOrNull()!!
+        val n = nums.size
+        val max = nums.maxOrNull()!!
+        val posList = ArrayList<Int>()
+        posList.add(-1)
+        for (i in 0 until n) {
+            if (nums[i] == max) {
+                posList.add(i)
+            }
+        }
+        var left = 1
+        var right = k
         var ans = 0L
-        var c = 0
-        var l = 0
-        for (item in nums) {
-            if (item == maxItem) {
-                c++
-            }
-            while (c >= k) {
-                if (nums[l++] == maxItem) c--
-            }
-            ans += l
+        while (right < posList.size) {
+            ans += (posList[left] - posList[left - 1]).toLong() * (n - posList[right])
+            left++
+            right++
         }
         return ans
     }
+
+//    fun countSubarrays(nums: IntArray, k: Int): Long {
+//        val maxItem = nums.maxOrNull()!!
+//        var ans = 0L
+//        var c = 0
+//        var l = 0
+//        for (item in nums) {
+//            if (item == maxItem) {
+//                c++
+//            }
+//            while (c >= k) {
+//                if (nums[l++] == maxItem) c--
+//            }
+//            ans += l
+//        }
+//        return ans
+//    }
 }
