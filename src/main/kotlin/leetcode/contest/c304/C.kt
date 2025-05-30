7dad08c07acfc1d767de41ac03594537ab1d42a0
HaKu <335279429@qq.com>
2025-05-30 13:42:11 +0800

    update

--- src/main/kotlin/leetcode/contest/c304/C.kt
+++ src/main/kotlin/leetcode/contest/c304/C.kt
@@ -6,7 +6,8 @@ import utils.print
 
 fun main() {
     val s = SolutionC()
-    s.closestMeetingNode(intArrayOf(2, 2, 3, -1), 0, 1).print()
+//    s.closestMeetingNode(intArrayOf(2, 2, 3, -1), 0, 1).print()
+    s.closestMeetingNode(intArrayOf(5, 4, 5, 4, 3, 6, -1), 0, 1).print()
 }
 
 class SolutionC {
@@ -17,13 +18,17 @@ class SolutionC {
             if (edges[i] != -1)
                 g.addEdgeOri(i, edges[i], 1)
         }
-        var cur = Long.MAX_VALUE
+        var cur = Long.MAX_VALUE / 4
         var ans = -1
         val a = g.dijkstra(node1)
         val b = g.dijkstra(node2)
 
+//        a.print()
+//        b.print()
+
         for (i in 0 until n) {
             if (maxOf(a[i], b[i]) < cur) {
+//                println("$i: ${a[i]} ${b[i]} with ${maxOf(a[i], b[i])}")
                 ans = i
                 cur = maxOf(a[i], b[i])
             }
