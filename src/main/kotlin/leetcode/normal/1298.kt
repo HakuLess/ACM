4b24746786082162be278027369225ea9164b240
HaKu <335279429@qq.com>
2025-06-03 01:28:45 +0800

    add

---
+++ src/main/kotlin/leetcode/normal/1298.kt
@@ -0,0 +1,59 @@
+package leetcode.normal
+
+import java.util.*
+
+class Solution1298 {
+    fun maxCandies(
+        status: IntArray,
+        candies: IntArray,
+        keys: Array<IntArray>,
+        containedBoxes: Array<IntArray>,
+        initialBoxes: IntArray
+    ): Int {
+        val n = status.size
+        val hasBox = BooleanArray(n)
+        val hasKey = BooleanArray(n)
+        val opened = BooleanArray(n)
+
+        val queue: Queue<Int> = LinkedList<Int>()
+
+        for (box in initialBoxes) {
+            hasBox[box] = true
+            if (status[box] == 1) queue.offer(box)
+        }
+
+
+        var ans = 0
+
+        while (queue.isNotEmpty()) {
+            val box = queue.poll()
+            if (opened[box]) continue
+            opened[box] = true
+            ans += candies[box]
+
+            // 拿到钥匙
+            for (key in keys[box]) {
+                if (!hasKey[key]) {
+                    hasKey[key] = true
+                    if (hasBox[key] && !opened[key]) {
+                        queue.add(key)
+                    }
+                }
+            }
+
+            // 拿到盒子
+            for (contained in containedBoxes[box]) {
+                if (!hasBox[contained]) {
+                    hasBox[contained] = true
+                    if (status[contained] == 1 || hasKey[contained]) {
+                        queue.add(contained)
+                    }
+                } else if (hasKey[contained] && !opened[contained]) {
+                    queue.add(contained)
+                }
+            }
+        }
+
+        return ans
+    }
+}
