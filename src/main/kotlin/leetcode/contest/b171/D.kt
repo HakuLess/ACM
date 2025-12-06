9cf5499eded2f5751e7a253a9411cc54e46fbd32
HaKu <hakuless@gmail.com>
2025-12-06 23:22:31 +0800

    update

--- src/main/kotlin/leetcode/contest/b171/D.kt
+++ src/main/kotlin/leetcode/contest/b171/D.kt
@@ -1,6 +1,6 @@
 package leetcode.contest.b171
 
-import utils.gpt.FenwickTree
+import utils.SortedList
 import utils.print
 
 fun main() {
@@ -12,40 +12,71 @@ fun main() {
 
 class SolutionD {
     fun minInversionCount(nums: IntArray, k: Int): Long {
-
-        val n = nums.size
-
-        val sorted = nums.distinct().sorted()
-        fun compress(x: Int) = sorted.binarySearch(x) + 1
-        val maxC = sorted.size
-
-        val fenwick = FenwickTree(maxC)
-
-        var cur = 0L
-        var ans = Long.MAX_VALUE
+        val window = SortedList<Int>()
+        var inv = 0L
 
         for (i in 0 until k) {
-            val c = compress(nums[i])
-            cur += fenwick.countGT(c, i)
-            fenwick.update(c, 1)
+            val x = nums[i]
+            val greater = window.largerThanAndEqual(x + 1)
+            inv += greater
+            window.insert(x)
         }
 
-        ans = minOf(ans, cur)
+        var ans = inv
 
-        for (i in k until n) {
-            val cAdd = compress(nums[i])
-            val cRem = compress(nums[i - k])
+        for (i in k until nums.size) {
+            val add = nums[i]
+            val remove = nums[i - k]
 
-            cur -= fenwick.countLT(cRem)
-            fenwick.update(cRem, -1)
+            val smaller = window.smallerThanAndEqual(remove - 1)
+            inv -= smaller
+            window.remove(remove)
 
-            val windowSize = k - 1
-            cur += fenwick.countGT(cAdd, windowSize)
-            fenwick.update(cAdd, 1)
+            val greater = window.largerThanAndEqual(add + 1)
+            inv += greater
+            window.insert(add)
 
-            ans = minOf(ans, cur)
+            ans = minOf(ans, inv)
         }
 
         return ans
     }
+
+//    fun minInversionCount(nums: IntArray, k: Int): Long {
+//
+//        val n = nums.size
+//
+//        val sorted = nums.distinct().sorted()
+//        fun compress(x: Int) = sorted.binarySearch(x) + 1
+//        val maxC = sorted.size
+//
+//        val fenwick = FenwickTree(maxC)
+//
+//        var cur = 0L
+//        var ans = Long.MAX_VALUE
+//
+//        for (i in 0 until k) {
+//            val c = compress(nums[i])
+//            cur += fenwick.countGT(c, i)
+//            fenwick.update(c, 1)
+//        }
+//
+//        ans = minOf(ans, cur)
+//
+//        for (i in k until n) {
+//            val cAdd = compress(nums[i])
+//            val cRem = compress(nums[i - k])
+//
+//            cur -= fenwick.countLT(cRem)
+//            fenwick.update(cRem, -1)
+//
+//            val windowSize = k - 1
+//            cur += fenwick.countGT(cAdd, windowSize)
+//            fenwick.update(cAdd, 1)
+//
+//            ans = minOf(ans, cur)
+//        }
+//
+//        return ans
+//    }
 }
